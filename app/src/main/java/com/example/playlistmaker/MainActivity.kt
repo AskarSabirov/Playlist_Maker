60b5964d864c2c25de406acd7934bbe5ff45a507
Аскар <sabirov_askar@inbox.ru>
2024-09-25 23:36:51 +0500

    Домашнее задание

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.playlistmaker
-
+import android.content.Intent
+import android.widget.Toast
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,12 +13,29 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button = findViewById<Button>(R.id.search)
+
+        val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val buttonIntent = Intent(this@MainActivity, Search_Activity::class.java)
+                startActivity(buttonIntent)
+            }
+        }
+
+        button.setOnClickListener(buttonClickListener)
+
+        val mediaButton = findViewById<Button>(R.id.media)
+        mediaButton.setOnClickListener {
+            val mediaButtonIntent = Intent(this, Media_Activity::class.java)
+            startActivity(mediaButtonIntent)
+        }
+
+        val settingsButton = findViewById<Button>(R.id.settings)
+        settingsButton.setOnClickListener {
+            val settingsButtonIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsButtonIntent)
         }
     }
 }
